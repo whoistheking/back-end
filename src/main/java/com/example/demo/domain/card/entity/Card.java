2d59bba2597e6e10bf8428b095e1c7d2e0180c39
stoow1 <spacegle12341@gmail.com>
2024-02-22 07:01:11 -0500

    feat : 카드 조회, 중복 제거, 셔플 , 배분

---
+++ src/main/java/com/example/demo/domain/card/entity/Card.java
@@ -0,0 +1,31 @@
+package com.example.demo.domain.card.entity;
+
+import com.example.demo.domain.user.entity.User;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import static lombok.AccessLevel.PROTECTED;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = PROTECTED)
+public class Card {
+
+    @Id
+    @Column(name = "card_id")
+    private Long cardId;
+
+//    @Column(name = "card")
+//    private boolean cardNum;    //???
+
+    @Column(name = "suit")
+    private String suit;
+
+    @Column(name = "card_num")
+    private String cardNum;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+}
