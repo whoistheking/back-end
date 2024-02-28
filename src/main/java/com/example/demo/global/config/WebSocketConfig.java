0815629c92a9a1f7280a625014bee2aa55763098
K-IMjihun <kjh950901@gmail.com>
2024-02-28 16:46:25 +0900

    refactor: 기타 코드 수정

--- src/main/java/com/example/demo/global/config/WebSocketConfig.java
+++ src/main/java/com/example/demo/global/config/WebSocketConfig.java
@@ -7,18 +7,15 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.messaging.support.ChannelInterceptor;
-import org.springframework.web.socket.config.annotation.*;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 @Configuration
-//@EnableWebSocket
 @RequiredArgsConstructor
 @EnableWebSocketMessageBroker
 @Slf4j
-//public class WebSocketConfig implements WebSocketConfigurer {
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
-
-    private final MyTextHandler webTextHandler;
     private final StompHandler stompHandler;
 
 
